f3ae6201f5e1ac3eeca71787eeea83ca820100a0
Taylor Bryant <taylor@bryant.io>
2019-05-27 15:17:24 -0500

    Add font-sans to layout component

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import "../style.css";
 
 function Layout(props) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col font-sans min-h-screen">
       <Header />
       <main className="flex-1 max-w-4xl mx-auto p-4 md:px-8 md:py-16 w-full">
         {props.children}
