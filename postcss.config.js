5d0f6eb8bff83902a2ef4750dcae8fd147491c08
Taylor Bryant <taylor@bryant.io>
2020-01-16 21:46:27 -0600

    Use Next.js's new built-in CSS support

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,16 @@
+/* eslint-env node */
 module.exports = {
-  plugins: [
-    require("tailwindcss"),
+  plugins: {
+    tailwindcss: {},
     ...(process.env.NODE_ENV === `production`
-      ? [
-          require("@fullhuman/postcss-purgecss")({
-            content: ["./src/**/*.js"],
+      ? {
+          "@fullhuman/postcss-purgecss": {
+            content: [`./src/**/*.js`],
             defaultExtractor: content =>
               content.match(/[A-Za-z0-9-_:/]+/g) || []
-          }),
-          require("autoprefixer"),
-          require("cssnano")
-        ]
-      : [])
-  ]
+          },
+          autoprefixer: {}
+        }
+      : {})
+  }
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,11 @@
 import Header from "./header";
 import Footer from "./footer";
-import "../style.css";
 
 function Layout(props) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 max-w-4xl mx-auto p-4 md:px-8 md:py-16 w-full">
+      <main className="flex-1 w-full max-w-4xl p-4 mx-auto md:px-8 md:py-16">
         {props.children}
       </main>
       <Footer />
