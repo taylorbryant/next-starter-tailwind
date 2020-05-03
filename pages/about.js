79a9fec2633e86c9fcc705cdeda1d3ffdec7c564
Taylor Bryant <taylor@bryant.io>
2020-05-03 13:11:10 -0500

    Fix missing key error on About page

--- pages/about.js
+++ pages/about.js
@@ -1,36 +1,40 @@
 import Layout from "../components/layout";
 
-function About() {
+function AboutPage() {
   return (
     <Layout>
-      <div className="flex flex-col md:flex-row">
-        <div className="md:w-1/2">
+      <div className="flex flex-col space-y-6 md:flex-row md:space-x-6 md:space-y-0">
+        <div className="space-y-6 md:w-1/2">
           {[
             {
               heading: `What is Tailwind?`,
               body: `Tailwind CSS is a highly customizable, low-level CSS framework that gives you all
               of the building blocks you need to build bespoke designs without any
-              annoying opinionated styles you have to fight to override.`
+              annoying opinionated styles you have to fight to override.`,
             },
             {
               heading: `What is Next.js?`,
               body: `Next.js is a minimalistic framework for creating server-rendered
-              React applications.`
-            }
-          ].map(section => (
-            <>
-              <h2 className="font-bold mb-3 text-xl">{section.heading}</h2>
-              <p className="mb-6">{section.body}</p>
-            </>
+              React applications.`,
+            },
+          ].map((section) => (
+            <div key={section.heading}>
+              <h2 className="mb-3 text-xl font-bold">{section.heading}</h2>
+              <p>{section.body}</p>
+            </div>
           ))}
         </div>
 
-        <div className="md:ml-6 md:w-1/2">
-          <img src="critter.svg" className="w-full" />
+        <div className="md:w-1/2">
+          <img
+            alt="A one-eyed alien holding a broken cable connected between a server and a desktop computer"
+            className="w-full"
+            src="critter.svg"
+          />
         </div>
       </div>
     </Layout>
   );
 }
 
-export default About;
+export default AboutPage;
