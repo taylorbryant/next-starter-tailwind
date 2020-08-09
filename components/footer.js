53fd9fb1215bde7566725e04b3022250cc0c3049
Taylor Bryant <taylor@bryant.io>
2020-08-09 17:43:55 -0500

    Add rustywind npm script to sort Tailwind CSS classes (#114)

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="bg-blue-500">
-      <ul className="flex items-center justify-between max-w-4xl mx-auto p-4 md:p-8 text-sm text-white">
+      <ul className="flex items-center justify-between max-w-4xl p-4 mx-auto text-sm text-white md:p-8">
         <li>
           Created by{" "}
           <a href="https://bryant.io" target="_blank" className="font-bold">
