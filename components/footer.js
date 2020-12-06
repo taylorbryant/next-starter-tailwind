01922fa7855ee0a2729741a3ca22a2c9d442debd
Taylor Bryant <taylor@bryant.io>
2020-12-06 15:49:19 -0600

    Improve contrast ratio in footer

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-blue-500">
+    <footer className="bg-blue-600">
       <ul className="flex items-center justify-between lg:container px-4 py-6 mx-auto text-sm text-white md:px-6">
         <li>
           Created by{" "}
