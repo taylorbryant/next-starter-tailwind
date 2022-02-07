a841a4d8b1adbeb5e238a21c85cf6e7eaec63254
Taylor Bryant <taylor@bryant.io>
2022-02-06 21:48:41 -0600

    next-starter-tailwind v3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import Head from "next/head";
-import Layout from "@components/layout";
+import Layout from "@components/Layout";
 
 function App({ Component, pageProps }) {
   return (
