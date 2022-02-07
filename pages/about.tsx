08922edfd819de597d329829ae2e5e40985e6ce1
Taylor Bryant <taylor@bryant.io>
2022-02-06 22:48:13 -0600

    Fix

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,40 +1,35 @@
 import Image from "next/image";
-import { NextSeo } from "next-seo";
 
 export default function AboutPage() {
   return (
-    <>
-      <NextSeo title="About" />
+    <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+      <div>
+        <section className="mb-6">
+          <h2 className="mb-3 text-xl font-bold">What is Tailwind?</h2>
 
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-        <div>
-          <section className="mb-6">
-            <h2 className="mb-3 text-xl font-bold">What is Tailwind?</h2>
+          <p>
+            Tailwind CSS is a highly customizable, low-level CSS framework that
+            gives you all of the building blocks you need to build bespoke
+            designs without any annoying opinionated styles you have to fight to
+            override.
+          </p>
+        </section>
 
-            <p>
-              Tailwind CSS is a highly customizable, low-level CSS framework
-              that gives you all of the building blocks you need to build
-              bespoke designs without any annoying opinionated styles you have
-              to fight to override.
-            </p>
-          </section>
-
-          <section>
-            <h2 className="mb-3 text-xl font-bold">What is Next.js?</h2>
-            <p>
-              Next.js is a minimalistic framework for creating server-rendered
-              React applications.
-            </p>
-          </section>
-        </div>
-
-        <Image
-          alt="A one-eyed alien holding a broken cable connected between a server and a desktop computer"
-          src="/critter.svg"
-          width={476}
-          height={297.17}
-        />
+        <section>
+          <h2 className="mb-3 text-xl font-bold">What is Next.js?</h2>
+          <p>
+            Next.js is a minimalistic framework for creating server-rendered
+            React applications.
+          </p>
+        </section>
       </div>
-    </>
+
+      <Image
+        alt="A one-eyed alien holding a broken cable connected between a server and a desktop computer"
+        src="/critter.svg"
+        width={476}
+        height={297.17}
+      />
+    </div>
   );
 }
